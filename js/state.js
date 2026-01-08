80a893dc9ebff2609ed3d5430a39e29a75ea4d5b
Yagi2025-12-29 <jamadawa2015@gmail.com>
2026-01-08 23:23:03 +0900

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,19 +1,21 @@
-const step1 = {
-  faceCut: false,
-  zZero: false,
-  finishUsed: false,
-  badRpm: false
+export const machineState = {
+  realPos: { x: 200, z: 600, s: 0 },
+  zeroRef: { x: 200, z: 600, s: 0 },
+
+  isFeedOn: false,
+  isHalfNutOn: false,
+  threadMoveDir: 0,
+
+  accelValue: 0,
+  holdTimer: null,
+  holdInterval: null,
+
+  work: { d: 60, out: 46, grab: 12 },
+
+  step1: {
+    faceCut: false,
+    zZero: false,
+    finishUsed: false,
+    badRpm: false
+  }
 };
-
-let realPos = { x: 200, z: 600, s: 0 };
-let zeroRef = { x: 200, z: 600, s: 0 };
-
-let isFeedOn = false;
-let isHalfNutOn = false;
-let threadMoveDir = 0;
-
-let accelValue = 0;
-let holdTimer = null;
-let holdInterval = null;
-
-let work = { d: 60, out: 46, grab: 12 };
